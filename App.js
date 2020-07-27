29bb6790259a79a3214e51cc47c4c1dfa2382876
DiamondsZz <951311326@qq.com>
2020-07-28 00:56:20 +0800

    navigation first try

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import router from './app/route/route';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import RootTabs from './app/route/route';
+const Stack = createStackNavigator();
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 const App = () => {
-  return <View>{router()}</View>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 const styles = StyleSheet.create({});
