a1f73be6c3742c74e737e581ec63918fdaae3bbb
DiamondsZz <951311326@qq.com>
2020-08-01 11:40:08 +0800

    字体图标引入

--- app/route/route.js
+++ app/route/route.js
@@ -1,21 +1,49 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Index from '../views/Index/index';
 import Find from '../views/Find/index';
 import Order from '../views/Order/index';
 import Mine from '../views/Mine/index';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 const Tabs = createBottomTabNavigator();
 
 const RootTabs = () => {
   return (
     <NavigationContainer>
       <Tabs.Navigator>
-        <Tabs.Screen name="首页" component={Index} />
-        <Tabs.Screen name="发现" component={Find} />
-        <Tabs.Screen name="订单" component={Order} />
-        <Tabs.Screen name="我的" component={Mine} />
+        <Tabs.Screen
+          name="Home"
+          component={Index}
+          options={{
+            tabBarLabel: '首页',
+            tabBarIcon: () => <AntDesign name="home" size={16} />,
+          }}
+        />
+        <Tabs.Screen
+          name="Find"
+          component={Find}
+          options={{
+            tabBarLabel: '发现',
+            tabBarIcon: () => <AntDesign name="find" size={16} />,
+          }}
+        />
+        <Tabs.Screen
+          name="Order"
+          component={Order}
+          options={{
+            tabBarLabel: '订单',
+            tabBarIcon: () => <AntDesign name="filetext1" size={16} />,
+          }}
+        />
+        <Tabs.Screen
+          name="Mine"
+          component={Mine}
+          options={{
+            tabBarLabel: '我的',
+            tabBarIcon: () => <AntDesign name="user" size={16} />,
+          }}
+        />
       </Tabs.Navigator>
     </NavigationContainer>
   );
